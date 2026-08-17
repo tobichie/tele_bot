93b3f86eed1849c513e5be1f0b151bb917a9b967
Atarairit <tyron.obichie3@gmail.com>
2026-08-17 15:58:40 +0200

    idk

--- src/bot_cmd_handler.py
+++ src/bot_cmd_handler.py
@@ -10,7 +10,8 @@ from pathlib import Path
 # -src
 #   - telebot.py
 # - api.py
-# Only the contents of src are included for obvious reasons. To use your own api create a file names api.py in the same
+# - recv_list.txt
+# Only the contents of src and the recv_list.txt file are included for obvious reasons. To use your own api create a file names api.py in the same
 # directory as src and make it have a function that returns the api key
 
 bot = telebot.TeleBot(token=api())
@@ -28,12 +29,10 @@ def welcome(message):
         ids = [line.strip() for line in f]
 
         if str(message.chat.id) not in ids:
-            # log.log_new_start(message.chat.id, message.from_user.first_name, path)
             logger.logit("[START]", message.chat.id, message.from_user.first_name, reason="User started receiving signals", path_to_log=path)
             f.write(f"{message.chat.id}\n")
         else:
-            # user is already part of the signaling list
-            # log.log_bad_start(message.chat.id, message.from_user.first_name, path)
+            # user is already part of the receiving list
             logger.logit("[INFO]", message.chat.id, message.from_user.first_name, reason="User tried to start Bot but its already started", path_to_log=path)
 
 @bot.message_handler(commands=['help'])
@@ -56,50 +55,47 @@ def privacy(message):
     user_first_name = message.from_user.first_name
     user_last_name = message.from_user.last_name
     user_username = message.from_user.username
-    msg1 = f"You can find our privacy statement under xxx.xx/privacy.html \nThe statement contains information regarding the storage of user data \n In order to comply with local and state authority we have also included a short summary of the processes that are carried out on personal data."
-    msg2 = f"Telegram user IDs, Usernames"
-    msg3 = f"Telegram user IDs, Usernames, first and last names saved to the account and the email the user registers t"
-    msg4 = f"Data Retention Policy: \n"
-    msg5 = f"All your privacy related information is shown below. \nPlease note that this information is always being processed and sent to the bot by Telegram when users interact with it. \n As such we have no way to restrict this information. We do however minimize it to only use what is strictly required. For our data retention policy, please look to our privacy statement or the summary above."
-    msg6 = f"Username: {user_username}\nFirst Name: {user_first_name}\nLast Name: {user_last_name}\n Chat ID: {message.chat.id}"
 
-    messages.append(msg1)
-    messages.append(msg2)
-    messages.append(msg3) # I know I know this sucks ill load it from a different file soon just hold up
-    messages.append(msg4)
-    messages.append(msg5)
-    messages.append(msg6)
-    for msg in messages:
-        bot.send_message(
-            message.chat.id,
-            msg
-        )
+    msg = f"Username: {user_username}\nFirst Name: {user_first_name}\nLast Name: {user_last_name}\n Chat ID: {message.chat.id}"
 
+    bot.send_message(
+        message.chat.id,
+        msg
+    )
 
 @bot.message_handler(commands=['contact'])
 def contact(message):
     print("Showing Contact Information to", message.from_user.first_name)
     user = message.from_user.first_name
-    bot.send_message(message.chat.id, f"Your Username is: {user}")
+    bot.send_message(message.chat.id, f"Your Username is: {user}") # This is meant to include your busines' contact information, or it should exist at all
+
 @bot.message_handler(commands=['mail'])
 def add_to_email_list(message):
     bot.send_message(message.chat.id, 'Enter your email if you want to receive email notifications for signals.')
     bot.register_next_step_handler(message, process_email)
 
-
 def process_email(message):
     EMAIL_PATTERN = r"^[^@\s]+@[^@\s]+\.[^@\s]+$"
+    #-------------------------------------------
+    # use email_validator module instead
+    # from email_validator import validate_email
+    # email = "abcd8@gmail.com"
+    # res = validate_email(email) # throws error if it fails so try/catch it
+    # print("Valid Email")
+    # _________________DO THIS___________________
+
     if re.match(EMAIL_PATTERN, message.text):
         path = Path("../logs/bot.log")
         email = message.text
         bot.send_message(message.chat.id, f"Sending a confirmation email to: {email}")
         bot.send_message(message.chat.id, f"This Function is still under construction, you will not receive an email.")
-        # Save to your database here
+        # Save to database here
 
         # after that log the event
         logger.logit("[INFO]", message.chat.id, message.from_user.first_name, reason=f"The user registered under the email: {email}", path_to_log=path)
     else:
         bot.reply_to(message, "Please enter a valid email address.")
+
 @bot.message_handler()
 def reply_func(message):
     bot.reply_to(message, text="This is a reply")
